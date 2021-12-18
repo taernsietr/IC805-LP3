70c9159068c76f150dfddbdd0d36266a56b5a870
Thalles Rodrigues <ts.rodr@gmail.com>
2021-12-18 00:53:05 -0300

    added gravity, cleaned vars

--- Fireworks.java
+++ Fireworks.java
@@ -1,4 +1,5 @@
-import processing.core.*;
+import processing.core.PApplet;
+import processing.core.PVector;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -9,22 +10,24 @@ public class Fireworks extends PApplet {
   // Animation parameters
   private int canvasWidth = 800;
   private int canvasHeight = 800;
+  private float maxSpeed = 3.0f;
+  private float maxAccel = 0.5f;
+  private PVector gravity = new PVector(0.00f, 0.05f);
 
+  // Main method
   public static void main(String[] args) {
     String[] processingArgs = { "Fireworks" };
     Fireworks fireworks = new Fireworks();
     PApplet.runSketch(processingArgs, fireworks);
   }
 
+  // Processing methods
   public void settings() {
     size(canvasWidth, canvasHeight);
   }
 
   public void mousePressed() {
     this.objs.add(new Mover(mouseX, mouseY, 1.0f, 10.0f));
-    // Debug
-    if (debug)
-      System.out.println("New Mover created at " + mouseX + ", " + mouseY);
   }
 
   public Fireworks() {
@@ -37,16 +40,14 @@ public class Fireworks extends PApplet {
     fill(200, 200, 200);
     for(int i = 0; i < objs.size(); i++) {
       Mover current = objs.get(i);
-      current.setMaxAccel(0.0f);
-      current.setMaxSpeed(0.0f);
-      current.force(0.00f, 0.00f);
+      current.setMaxAccel(maxAccel);
+      current.setMaxSpeed(maxSpeed);
+      current.force(gravity);
       current.update();
       ellipse(current.pos.x,
               current.pos.y,
               current.size,
               current.size);
-      if (debug)
-        System.out.println("Obj 1: " + current.pos.x + ", " + current.pos.y + ", " + current.spd + ", " + current.acc);
     }
   }
 }
