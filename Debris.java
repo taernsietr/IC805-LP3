03cb7d273cb14553c6164a17dc449146ec7d0945
Thalles Rodrigues <ts.rodr@gmail.com>
2021-12-19 18:14:56 -0300

    n lembro onde parei

--- Debris.java
+++ Debris.java
@@ -17,8 +17,8 @@ public class Debris extends Mover {
   // Sets time to live
   // lifetime should be given in _seconds_
   public void setLifetime(float lifetime) {
-    this.lifetime = (lifetime + this.mass - 1) * 60.0f;
-    this.ttl = lifetime;
+    this.lifetime = lifetime * 60.0f;
+    this.ttl = this.lifetime;
   }
 
   public float getTTL() {
@@ -29,14 +29,21 @@ public class Debris extends Mover {
     this.spd.add(this.acc);
     this.spd.limit(maxSpeed);
     this.pos.add(this.spd);
-    this.ttl -= 0.015f;
+    this.ttl -= 0.01f;
   }
 
   public void draw() {
     this.p.noStroke();
-    this.p.fill(this.p.map(this.ttl, 0.0f, this.lifetime * 0.03f, 40, 255), 40, 20);
+    this.p.fill(
+        this.p.map(
+          this.ttl,
+          0.0f,
+          this.lifetime * (this.mass / this.size) * 10.0f,
+          10,
+          255),
+        10, 10);
     this.p.circle(this.pos.x, this.pos.y, this.size);
-    System.out.println(this.ttl + " " + this.lifetime);
+    //System.out.println(this.ttl + " " + this.lifetime);
   }
 
 }
--- Fireworks.java
+++ Fireworks.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Fireworks extends PApplet {
+  private Random rand = new Random();
   private ArrayList<Mover> movers = new ArrayList<Mover>();
   private ArrayList<Debris> debris = new ArrayList<Debris>();
 
@@ -26,16 +27,29 @@ public class Fireworks extends PApplet {
   }
 
   public void mousePressed() {
+    for(int j = 0; j < 30; j++) {
+      Debris e = new Debris(this,
+      rand.nextFloat(0.0f, canvasWidth-30.0f),
+      rand.nextFloat(0.0f, 30.0f), 
+      rand.nextFloat(2.0f, 50.0f),
+      rand.nextFloat(2.0f, 20.0f));
+      e.setLifetime(2.0f);
+      this.debris.add(e);
+    }
+  }
+/*
     Debris d = new Debris(this, mouseX, mouseY, 1.0f, 10.0f);
     d.setMaxAccel(maxAccel);
     d.setMaxSpeed(maxSpeed);
     d.setLifetime(2.0f);
     this.debris.add(d);
+
+  public Fireworks() {
   }
-
+*/
   public void draw() {
+    System.out.println(debris.size());
     background(0, 0, 0);
-
     for(int i = 0; i < debris.size(); i++) {
       Debris d = debris.get(i);
       d.force(gravity);
